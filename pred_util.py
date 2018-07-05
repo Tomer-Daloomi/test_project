ad5e2a33b053e79d15b907366d1533404bcc0d30
Tomer-Daloomi <tomerdaloomi@gmail.com>
2018-07-05 13:46:25 +0300

    the code is complete, only regression and classification function from sklearn don't work yet. needs slight debugging.

--- pred_util.py
+++ pred_util.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn import linear_model
 
 
 def cleaned_data(address):
@@ -42,3 +44,99 @@ def cleaned_data(address):
     df = df[df["test result"] != 0]
 
     return df
+
+
+def features_and_labels(data, rare_test, tests_array):
+    """
+    the function creates a bundle of n feature vectors, a vector for every patient that took the
+    "rare test", each vector is of length d.
+
+    :param data:
+    :param rare_test:
+    :param tests_array:
+    :return: n X d matrix of features, and n X 1 vector of labels
+    """
+
+    # first we create a list of the patients that have taken the rare test
+    # and a labels vector with the rare test results
+
+    rare_patients = []
+    y = []
+
+    for index, row in data.iterrows():
+        if row['test id'] == rare_test:
+            rare_patients.append(row['patient id'])
+            y.append(row['test result'])
+
+    # then, we create a feature vector for each of these patients, containing their age,
+    # and all their test result, out of the test that are enlisted in tests_dic
+
+    features_matrix = []
+
+    for patient in rare_patients:
+
+        feature_vector = []
+        patient_df = data[data['patient id'] == patient]
+
+        # first we add the patient's age to the feature vector
+        feature_vector.append(patient_df['age'][patient_df['age'].first_valid_index()])
+
+        # then we add the relevant tests results to the vector
+        patient_tests_dic = pd.Series(patient_df['test result'].values, index=patient_df[
+            'test id']).to_dict()
+
+        for test in tests_array:
+            if test in patient_tests_dic:
+                feature_vector.append(patient_tests_dic[test])
+            else:
+                feature_vector.append(0)
+
+        features_matrix.append(feature_vector)
+
+    X = np.matrix(features_matrix)
+    y = np.array(y).T
+
+    return X, y
+
+
+def logistic_regression(C, X, y):
+
+    log_reg = linear_model.LogisticRegression(C, multi_class='ovr')
+    # we shall create a Nearest Neighbors classifier
+    log_reg.fit(X, y)
+
+    return log_reg
+
+
+def lasso_regression(alpha, X_tr, y_tr):
+
+    lasso = linear_model.Lasso(alpha=0.1)
+    lasso.fit(X_tr, y_tr)
+
+    return lasso
+
+
+def linear_regression(X_tr, y_tr):
+
+    lin_reg = linear_model.LinearRegression()
+    lin_reg.fit(X_tr, y_tr)
+
+    return lin_reg
+
+
+def loss_0_1(h, X, y):
+    """
+    computes the test error using the 0-1 loss function
+
+    :param h: d X 1 matrix, where d- is the dimension of the hypothesis vector
+    :param X: n X d matrix with the samples
+    :param y: d X 1 matrix with the labels of the samples
+    :return:  0-1 loss for the hypothesis. (sign of inner product as label)
+    """
+    n = X.shape[0]
+
+    pred = np.inner(X, h)
+    diff = sum([1 if y[j] != pred[j] else 0 for j in range(n)])
+    error = diff / n
+
+    return error
--- predictor.py
+++ predictor.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 RARENESS = 1000
-
+TR = 0.7
+C = 1e5
+ALPHA = 0.1
 
 data = pu.cleaned_data('data_sample.csv')
 
@@ -14,21 +16,52 @@ data = pu.cleaned_data('data_sample.csv')
 # been taken more times than the chosen one - as the features. if a patient hasn't done a
 # certain test, it's value in the feature vector would be 0.
 
-times_taken = data["test id"].value_counts()
-least_common = times_taken[times_taken >= RARENESS]
-rare_test = least_common[-1:-2:-1]
-feature_tests = least_common[:-1:]
-tests_dic = feature_tests.to_dict()
-print(tests_dic, rare_test)
-# least_common_df = df.loc[df["test id"] == least_common]
+times_taken = data["test id"].value_counts()  # ordered from top to bottom
+more_common = times_taken[times_taken >= RARENESS]
 
-# print(np.shape(counts))
-# print(df['test result'])
-# print(df.describe(include='all'))
+rare_test = np.array(more_common[-1:-2:-1].index)  # the id str of the chosen rare test
+tests_array = np.array(more_common[:-1:].index)  # all the id's of the tests that were taken
+# more often than the 'rare test' as keys
 
-# now that we have cleaned and organized our data - we would like to create a feature vector for
-# each patient. the features are of course the different tests. in order to do so - we would
-# establish for each patient a dictionary of the tests he has done in which their value would be
-# the test result, and a dictionary of all tests, in which their value would be their mean value
-# over all patients. this way, when establishing the feature vectors - we would substitute the
-# mean value of a test result if the patient hasn't done it.
+X, y = pu.features_and_labels(data, rare_test, tests_array)
+
+# It is now time for training and testing! let us begin with putting 30% of the data aside for
+# testing, and keeping 70% for training
+
+n = np.shape(X)[0]
+
+X_tr = X[0:int(TR * n)]
+X_tst = X[int(TR * n):]
+
+y_tr = y[0:int(TR * n)]
+y_tst = y[int(TR * n):]
+
+# we would like to check, whether the y values could be any number within some limits, or if they
+# could only get the value of a discrete group of test results (i.e. a rational number between
+# [1,100] or an int in the group {0, 1, 2, 3, 4}).
+
+# in order to determine - we should use the threshold value 0.5 for repetitiveness of labels,
+# which effectively means:
+num_of_unique_labels = len(np.unique(y))
+num_of_labels = len(y)
+discrete = True if (num_of_unique_labels / num_of_labels) < 0.5 else False
+
+
+H = []
+# if the labels could only get few discrete values, we should obviously treat our problem as a
+# classification problem.
+if discrete:
+    H.append(pu.logistic_regression(C, X_tr, y_tr))
+# otherwise - it should be treated as a regression problem
+else:
+    H.append(pu.lasso_regression(ALPHA, X_tr, y_tr))
+    H.append(pu.linear_regression(X_tr, y_tr))
+
+
+# in order to check how well does the new hypothesis work, we shall calculate the error:
+for h in H:
+    tr_error = pu.loss_0_1(h, X_tr, y_tr)
+    tst_error = pu.loss_0_1(h, X_tst, y_tst)
+    print('training error: ', tr_error, ' test error: ', tst_error)
+
+
